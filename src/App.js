d2ced388fd92d6ec3b0da81fe31ce6036213a019
isiagi <isiagigeofrey0@gmail.com>
2021-05-05 01:56:49 +0300

    add feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import Header from "./components/header/Header";
 import Body from './components/body/Body';
 import Footer from './components/footer/Footer';
-import Header from "./components/header/Header";
 import Last from './components/last/Last';
+import './App.css';
 
 function App() {
   return (
